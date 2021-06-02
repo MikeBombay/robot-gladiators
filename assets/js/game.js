839ecdf3984f3c16804a3a0b40d49ea3550a4e61
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-06-02 16:22:10 -0400

    stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,17 +3,13 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+var fight = function(enemyName) {
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) { 
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -60,5 +56,12 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     fight();
   }
 }
+  }
 }
-fight();
+
+ 
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
+}
